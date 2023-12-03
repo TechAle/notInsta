df430843aba682a45900525244d8dcfcff82d88d
CamillaTedde <128591268+CamillaTedde@users.noreply.github.com>
2023-12-03 10:20:56 +0100

    Past project added

---
+++ TestJava/app/src/main/java/com/example/testjava/UI/post_creation/ImageFilterActivity.java
@@ -0,0 +1,14 @@
+package com.example.testjava.UI.post_creation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.testjava.R;
+
+public class ImageFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_filter);
+    }
+}
