360948e45ea188e9addf350dd46330a55adf0c97
CamillaTedde <128591268+CamillaTedde@users.noreply.github.com>
2023-12-03 11:51:28 +0100

    Take picture or choose from gallery

--- TestJava/app/src/main/java/com/example/testjava/UI/post_creation/TestActivity.java
+++ TestJava/app/src/main/java/com/example/testjava/UI/post_creation/TestActivity.java
@@ -2,6 +2,8 @@ package com.example.testjava.UI.post_creation;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.testjava.R;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class TestActivity extends AppCompatActivity {
 
+    int SELECT_PICTURE = 200;
+    int TAKE_PICTURE = 100;
     private ImageView imageView;
     private Button button;
 
@@ -27,7 +34,17 @@ public class TestActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(open_camera, 100);
+                startActivityForResult(open_camera, TAKE_PICTURE);
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), 200);
             }
         });
     }
@@ -35,9 +52,24 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = null;
 
         if (data != null){
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            if (requestCode == SELECT_PICTURE){
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+
+                    InputStream inputStream;
+                    try {
+                        inputStream = getContentResolver().openInputStream(selectedImageUri);
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    photo = BitmapFactory.decodeStream(inputStream);
+                }
+            } else {
+                photo = (Bitmap) data.getExtras().get("data");
+            }
             imageView.setImageBitmap(photo);
         }
     }
