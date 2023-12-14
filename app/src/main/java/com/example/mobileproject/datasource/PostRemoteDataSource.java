a25149cfcb56fcbdcb225ed2d10b310dc3a48434
Yanhui Matteo <huyanhuimatteo@gmail.com>
2023-12-14 17:36:02 +0100

    Google services + RemoteDataSource

---
+++ app/src/main/java/com/example/mobileproject/datasource/PostRemoteDataSource.java
@@ -0,0 +1,9 @@
+package com.example.mobileproject.datasource;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class PostRemoteDataSource {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+}
