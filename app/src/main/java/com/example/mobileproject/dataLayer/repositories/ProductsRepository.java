55e5dc3796973b6dc2db6ee15c816e3f1d412d35
Gabriele <g.zotta@campus.unimib.it>
2024-02-02 16:07:33 +0100

    Classi API 2

--- app/src/main/java/com/example/mobileproject/dataLayer/repositories/ProductsRepository.java
+++ app/src/main/java/com/example/mobileproject/dataLayer/repositories/ProductsRepository.java
@@ -27,76 +27,37 @@ public class ProductsRepository implements IProductsRepository {
 
     private final Application application;
     private final StoreAPIService storeAPIService;
-    private final NewsDao newsDao;
     private final ResponseCallback responseCallback;
 
     public ProductsRepository(Application application, ResponseCallback responseCallback) {
         this.application = application;
-        this.storeAPIService = ServiceLocator.getInstance().getNewsApiService();
-        NewsRoomDatabase newsRoomDatabase = ServiceLocator.getInstance().getNewsDao(application);
-        this.newsDao = newsRoomDatabase.newsDao();
+        this.storeAPIService = ServiceLocator.getInstance().getProductsApiService();
         this.responseCallback = responseCallback;
     }
 
     @Override
     public void fetchProducts(int limit, long lastUpdate) {
 
-        long currentTime = System.currentTimeMillis();
+        Call<StoreApiResponse> storeResponseCall = storeAPIService.getProducts(limit);
 
-        if (currentTime - lastUpdate > FRESH_TIMEOUT) {
-            Call<StoreApiResponse> storeResponseCall = storeAPIService.getProducts(limit);
+        storeResponseCall.enqueue(new Callback<StoreApiResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<StoreApiResponse> call,
+                                   @NonNull Response<StoreApiResponse> response) {
 
-            storeResponseCall.enqueue(new Callback<StoreApiResponse>() {
-                @Override
-                public void onResponse(@NonNull Call<StoreApiResponse> call,
-                                       @NonNull Response<StoreApiResponse> response) {
-
-                    if (response.body() != null && response.isSuccessful()) {
-                        List<Product> productList = response.body().getProducts();
-                        saveDataInDatabase(productList);
-                    } else {
-                        responseCallback.onFailure(application.getString(R.string.error_retrieving_products));
-                    }
-                }
-
-                @Override
-                public void onFailure(@NonNull Call<StoreApiResponse> call, @NonNull Throwable t) {
-                    responseCallback.onFailure(t.getMessage());
-                }
-            });
-        } else {
-            Log.d(TAG, application.getString(R.string.data_read_from_local_database));
-            readDataFromDatabase(lastUpdate);
-        }
-    }
-
-
-    private void saveDataInDatabase(List<Product> productList) {
-        NewsRoomDatabase.databaseWriteExecutor.execute(() -> {
-            List<Product> allProducts = newsDao.getAll();
-
-
-            for (Product product : allProducts) {
-
-                if (productList.contains(product)) {
-                    productList.set(productList.indexOf(product), product);
+                if (response.body() != null && response.isSuccessful()) {
+                    List<Product> productList = response.body().getProducts();
+                } else {
+                    responseCallback.onFailure(application.getString(R.string.error_retrieving_products));
                 }
             }
 
-            List<Long> insertedNewsIds = newsDao.insertNewsList(productList);
-            for (int i = 0; i < productList.size(); i++) {
-                productList.get(i).setId(insertedNewsIds.get(i));
+            @Override
+            public void onFailure(@NonNull Call<StoreApiResponse> call, @NonNull Throwable t) {
+                responseCallback.onFailure(t.getMessage());
             }
-
-            responseCallback.onSuccess(productList, System.currentTimeMillis());
         });
-    }
 
-
-    private void readDataFromDatabase(long lastUpdate) {
-        NewsRoomDatabase.databaseWriteExecutor.execute(() -> {
-            responseCallback.onSuccess(newsDao.getAll(), lastUpdate);
-        });
     }
 
 
