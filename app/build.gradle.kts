0c97ff4c9927c72d1bb843a05b16e65d8e2917f6
Gabriele <g.zotta@campus.unimib.it>
2024-02-02 11:21:13 +0100

    Classi API (da aggiustare database e testare)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Import Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
