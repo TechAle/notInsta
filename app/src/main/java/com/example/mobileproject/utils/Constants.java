ade3a9004f6356f10ff5b39f232b4b27b5d548f1
TechAle <alessandro.condello.email@gmail.com>
2024-02-03 11:18:14 +0100

    Fixati errore

--- app/src/main/java/com/example/mobileproject/utils/Constants.java
+++ app/src/main/java/com/example/mobileproject/utils/Constants.java
@@ -6,7 +6,7 @@ package com.example.mobileproject.utils;
 public class Constants {
     public static final int PICK_IMAGE_REQUEST = 1;
 
-    public static final String NEWS_API_BASE_URL = "https://fakestoreapi.com/products";
+    public static final String NEWS_API_BASE_URL = "https://fakestoreapi.com/";
     public static final String TOP_HEADLINES_ENDPOINT = "products/";
     public static final String TOP_HEADLINES_LIMIT_PARAMETER = "limit";
     public static final int FRESH_TIMEOUT = 1000 * 60 * 60; // 1 hour in milliseconds
